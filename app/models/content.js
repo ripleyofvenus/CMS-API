3edd92a52212d8fab822943dd8971370dea4d7cd
sjacobs146 <susanjacobs@verizon.net>
2017-10-11 14:20:22 -0400

    Add Content model
    Add model to store user created content.

---
+++ app/models/content.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const contentSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  _owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+    transform: function (doc, ret, options) {
+      const userId = (options.user && options.user._id) || false
+      ret.editable = userId && userId.equals(doc._owner)
+      return ret
+    }
+  }
+})
+
+const Content = mongoose.model('Content', contentSchema)
+
+module.exports = Content
