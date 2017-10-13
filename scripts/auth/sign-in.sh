f6d3141bd4a3b66d96baab73120be7ee14bc8880
sjacobs146 <susanjacobs@verizon.net>
2017-10-13 14:30:57 -0400

    Make all scripts point to localhost
    Change all scripts to point to localhost instead of heroku because it
    was causing confusion.
    Susan and Stephanie worked on this together.

--- scripts/auth/sign-in.sh
+++ scripts/auth/sign-in.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# API="http://localhost:4741"
-API="http://sserpdrow-api.herokuapp.com"
+API="http://localhost:4741"
+# API="http://sserpdrow-api.herokuapp.com"
 URL_PATH="/sign-in"
 
 curl "${API}${URL_PATH}" \
--- scripts/auth/sign-up.sh
+++ scripts/auth/sign-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# API="http://localhost:4741"
-API="http://sserpdrow-api.herokuapp.com"
+API="http://localhost:4741"
+# API="http://sserpdrow-api.herokuapp.com"
 URL_PATH="/sign-up"
 
 curl "${API}${URL_PATH}" \
